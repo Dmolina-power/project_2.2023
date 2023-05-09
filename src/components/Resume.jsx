9c25396a0ad07de49a7442e15d324eec33a49b64
daniel molina <bigdpower@icloud.com>
2023-05-08 23:25:35 -0500

    starter files

---
+++ src/components/Resume.jsx
@@ -0,0 +1,149 @@
+import React from "react";
+import resumeFile from "../documents/devRes2023.pdf";
+
+const Resume = ({ classicHeader, darkTheme }) => {
+  const educationDetails = [
+    {
+      yearRange: "2019 - 2020",
+      title: "Full Stack Web Development",
+      place: "Triliogy/University of Arizona",
+      desc: "A 12-Week coding Bootcamp.The industry-driven curriculum is continually optimized to teach the most in-demand technologies, including JavaScript, HTML, CSS, jQuery, Bootstrap, Node.js, MySQL, MongoDB, React, and more.",
+    },
+    {
+      yearRange: "2023 - Current",
+      title: "Bachelor Degree",
+      place: "Full Sail University",
+      desc: "Computer Science bachelor's program build foundations in the design, development, and implementation of software-based solutions for business, entertainment, and consumer markets.",
+    },
+  ];
+
+  const experienceDetails = [
+    {
+      yearRange: "2021 - 2022",
+      title: "Web Development Instructor",
+      place: "PersevereNow",
+      desc: "Mentoring 15 junior developers, training them in the industries best practices of frontend development, app development, as well as back-end & mobile friendly.Created 20+ lectures, teaching junior developers various computer languages, libraries and frameworks.Working to reduce recidivism in the State of Arizona, bringing new talent to the technology industry.",
+    },
+    {
+      yearRange: "2022 - 2023",
+      title: "Web Developer",
+      place: "PIAG Museum",
+      desc: "Web site created and monitored for updates built on React, Nextjs, material UI, Bootstrap.",
+    },
+  ];
+
+  return (
+    <section
+      id="resume"
+      className={"section " + (darkTheme ? "bg-dark-1" : "")}
+    >
+      <div className={"container " + (classicHeader ? "" : "px-lg-5")}>
+        {/* Heading */}
+        <div className="position-relative d-flex text-center mb-5">
+          <h2
+            className={
+              "text-24  text-uppercase fw-600 w-100 mb-0 " +
+              (darkTheme ? "text-muted opacity-1" : "text-light opacity-4")
+            }
+          >
+            Summary
+          </h2>
+          <p
+            className={
+              "text-9 text-dark fw-600 position-absolute w-100 align-self-center lh-base mb-0 " +
+              (darkTheme ? "text-white" : "text-dark")
+            }
+          >
+            {" "}
+            Resume
+            <span className="heading-separator-line border-bottom border-3 border-primary d-block mx-auto" />
+          </p>
+        </div>
+        {/* Heading end*/}
+        <div className="row gx-5">
+          {/* My Education */}
+          <div className="col-md-6">
+            <h2
+              className={
+                "text-6 fw-600 mb-4 " + (darkTheme ? "text-white" : "")
+              }
+            >
+              My Education
+            </h2>
+            {educationDetails.length > 0 &&
+              educationDetails.map((value, index) => (
+                <div
+                  key={index}
+                  className={
+                    "bg-white  rounded p-4 mb-4 " +
+                    (darkTheme ? "bg-dark" : "bg-white border")
+                  }
+                >
+                  <p className="badge bg-primary text-2 fw-400">
+                    {value.yearRange}
+                  </p>
+                  <h3 className={"text-5 " + (darkTheme ? "text-white" : "")}>
+                    {value.title}
+                  </h3>
+                  <p className={darkTheme ? "text-primary" : "text-danger"}>
+                    {value.place}
+                  </p>
+                  <p className={"mb-0 " + (darkTheme ? "text-white-50" : "")}>
+                    {value.desc}
+                  </p>
+                </div>
+              ))}
+          </div>
+          {/* My Experience */}
+          <div className="col-md-6">
+            <h2
+              className={
+                "text-6 fw-600 mb-4 " + (darkTheme ? "text-white" : "")
+              }
+            >
+              My Experience
+            </h2>
+            {experienceDetails.length > 0 &&
+              experienceDetails.map((value, index) => (
+                <div
+                  key={index}
+                  className={
+                    "bg-white  rounded p-4 mb-4 " +
+                    (darkTheme ? "bg-dark" : "bg-white border")
+                  }
+                >
+                  <p className="badge bg-primary text-2 fw-400">
+                    {value.yearRange}
+                  </p>
+                  <h3 className={"text-5 " + (darkTheme ? "text-white" : "")}>
+                    {value.title}
+                  </h3>
+                  <p className={darkTheme ? "text-primary" : "text-danger"}>
+                    {value.place}
+                  </p>
+                  <p className={"mb-0 " + (darkTheme ? "text-white-50" : "")}>
+                    {value.desc}
+                  </p>
+                </div>
+              ))}
+          </div>
+        </div>
+        
+        <div className="text-center mt-5">
+          <a
+            className="btn btn-outline-secondary rounded-pill shadow-none"
+            href={resumeFile}
+            download
+          >
+            Download CV
+            <span className="ms-1">
+              <i className="fas fa-download" />
+            </span>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
